b99ce9b218fb9785aa25f0c1161345b058fd82df
Crihalix <Crihalix@gmail.com>
2014-08-31 00:18:05 +0300

    final-task

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-final-task
-==========
+# task-name
+
+Description
--- scripts/final_task.js
+++ scripts/final_task.js
@@ -1,18 +1,35 @@
 
 function tabs(){
 
-	var tabs = document.getElementsByClassName("tab-item"),
-		pages = document.getElementsByClassName("tab-item-cnt"),
+	var tabs = document.getElementsByClassName('tab-item'),
+		pages = document.getElementsByClassName('tab-item-cnt'),
 		current = 0;
 
 	[].forEach.call(tabs, function(tab, index){
-		tab.addEventListener("click", function(event){
-			tabs[current].classList.remove("active");
-			pages[current].classList.remove("active");
-			tabs[index].classList.add("active");
-			pages[index].classList.add("active");
+		tab.addEventListener('click', function(){
+			tabs[current].classList.remove('active');
+			pages[current].classList.remove('active');
+			tabs[index].classList.add('active');
+			pages[index].classList.add('active');
 			current = index;					
 		});
 	});
 };
 tabs();
+
+function libra(){
+	var leftCup = document.getElementsByClassName('discript-left-cup'),
+		rightCup = document.getElementsByClassName('discript-right-cup'),
+		libra = document.getElementsByClassName('cups-block');
+
+	leftCup[0].addEventListener('click', function(event){
+		libra[0].classList.add('outweighed-left');
+		libra[0].classList.remove('outweighed-right');
+	});
+	rightCup[0].addEventListener('click', function(event){
+		libra[0].classList.add('outweighed-right');
+		libra[0].classList.remove('outweighed-left');
+	});
+
+};
+libra();
